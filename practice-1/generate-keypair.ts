3216856d7e916b0dd9d775bcf9cf8c83948a8c55
olegl <lutsykolehua@gmail.com>
2024-08-13 14:38:09 +0300

    moved the generate-keypair.ts file to the key-grinder folder

--- practice-1/generate-keypair.ts
+++ practice-1/generate-keypair.ts
@@ -1,38 +1,5 @@
 import { Keypair } from "@solana/web3.js";
-
-function checkPrefix(keypair, prefixes) {
-  const publicKey = keypair.publicKey.toBase58();
-  return prefixes.some((prefix) => publicKey.startsWith(prefix));
-}
-
-function findKeyWithPrefix(prefixes) {
-  let keypair;
-  let attempt = 0;
-  do {
-    keypair = Keypair.generate();
-    attempt++;
-    console.log(
-      `Attempt ${attempt}: Current public key: ${keypair.publicKey.toBase58()}`
-    );
-  } while (!checkPrefix(keypair, prefixes));
-
-  console.log(`✅ Found matching key after ${attempt} attempts!`);
-  return keypair;
-}
-
-const prefixes = [
-  "Oleg",
-  "oLeg",
-  "oleG",
-  "0leg",
-  "0leq",
-  "oleq",
-  "oleg",
-  "anza",
-  "anZa",
-];
-
-const keypair = findKeyWithPrefix(prefixes);
+const keypair = Keypair.generate();
 
 console.log(`The public key is: `, keypair.publicKey.toBase58());
 console.log(`The secret key is: `, keypair.secretKey);
